19bb9275d84a70c40bf04a737d5326cd39e8e6d2
WillSlye <40750143+WillSlye@users.noreply.github.com>
2023-02-20 16:53:53 -0500

    Update checkmypass.py

--- checkmypass.py
+++ checkmypass.py
@@ -3,10 +3,10 @@ import hashlib
 import sys
 
 def request_api_data(query_char):
-    url = 'https://api.pwnedpasswords.com/range/' + query_char
+    url = f'https://api.pwnedpasswords.com/range/{query_char}'
     res = requests.get(url)
     if res.status_code != 200:
-        raise RuntimeError(f"Error fetching: {res.status_code}, check the api and try again.")
+        raise RuntimeError(f'Error fetching: {res.status_code}, check the api and try again.')
     return res
 
 def get_password_leaks_count(hashes, hash_to_check):
@@ -16,50 +16,30 @@ def get_password_leaks_count(hashes, hash_to_check):
             return count
     return 0
 
-def pwned_api_check(password):#checks to see if the password exists in the API response
+def check_password(password):
     sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
     first5_char, tail = sha1password[:5], sha1password[5:]
     response = request_api_data(first5_char)
-    return get_password_leaks_count(response, tail)
+    count = get_password_leaks_count(response, tail)
+    return count
 
-def main(args):
-    for password in args:
-        count = pwned_api_check(password)
+def check_passwords(passwords):
+    results = []
+    for password in passwords:
+        count = check_password(password)
         if count:
-            print(f"{password} was found {count} times... You should probably change your password.")
+            results.append(f"{password} was found {count} times... You should probably change your password.")
         else:
-            print(f"{password} was NOT found! Good password!")
-    return 'done!'
+            results.append(f"{password} was NOT found! Good password!")
+    return results
+
+def main():
+    args = sys.argv[1:]
+    results = check_passwords(args)
+    for result in results:
+        print(result)
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    main()
 
 # end of file
